fa6b87b0957a20468215d7a7e42009498249affe
jesuscova <jesuscovam@icloud.com>
2020-03-10 12:12:39 -0500

    ¸created auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ActivitiesModule } from './activities/activities.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeConfig),
-    ActivitiesModule,],
+    ActivitiesModule,
+    AuthModule,],
 })
 export class AppModule {}
