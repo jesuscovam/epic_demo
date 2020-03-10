0764b866868431ecaff73185e77d7433d7ae20fa
jesuscova <jesuscovam@icloud.com>
2020-03-10 12:11:42 -0500

    deleted for redo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ActivitiesModule } from './activities/activities.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeConfig } from './config/typeorm.config';
-import { AuthModule } from './auth/auth.module';
+
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeConfig),
-    ActivitiesModule,
-    AuthModule],
+    ActivitiesModule,],
 })
 export class AppModule {}
